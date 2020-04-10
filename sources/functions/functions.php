b2379e789b091cb5780a225b65cc18480e53cae5
igweze <igweze@gmail.com>
2020-04-10 22:31:25 +0100

    Add files via upload

--- sources/functions/functions.php
+++ sources/functions/functions.php
@@ -6035,5 +6035,8 @@ END;
 		}		
 		 
 
-		
+		function validateMail($str) {
+			return (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? FALSE : TRUE;
+		}
+   
 ?>
