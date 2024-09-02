b425b599a0c07af86ce8b8ad743b8d2374d6f6f2
igweze <igweze@gmail.com>
2024-09-02 16:31:04 +0100

    Delete sources directory

--- sources/global/scripts/validate-pages.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-		if (!defined('wizGrade'))
-					
-	  			die('Hahahaha, Hacking attempt . . . . Be Careful . . . . You Are Been Warned. 
-					However, your ACTIVITIES are been MONITORED!!!!');
-
-?>
